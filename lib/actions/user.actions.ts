d0765de6b97c8f5079533e8288f62eed7cde045f
Alg1t <alfell88@googlemail.com>
2025-05-26 14:01:32 +0100

    paypal3

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -204,36 +204,36 @@ export async function updateUserPaymentMethod(
   }
 }
 
-// // Update the user profile
-// export async function updateProfile(user: { name: string; email: string }) {
-//   try {
-//     const session = await auth();
+// Update the user profile
+export async function updateProfile(user: { name: string; email: string }) {
+  try {
+    const session = await auth();
 
-//     const currentUser = await prisma.user.findFirst({
-//       where: {
-//         id: session?.user?.id,
-//       },
-//     });
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        id: session?.user?.id,
+      },
+    });
 
-//     if (!currentUser) throw new Error("User not found");
+    if (!currentUser) throw new Error("User not found");
 
-//     await prisma.user.update({
-//       where: {
-//         id: currentUser.id,
-//       },
-//       data: {
-//         name: user.name,
-//       },
-//     });
+    await prisma.user.update({
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name: user.name,
+      },
+    });
 
-//     return {
-//       success: true,
-//       message: "User updated successfully",
-//     };
-//   } catch (error) {
-//     return { success: false, message: formatError(error) };
-//   }
-// }
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
 
 // // Get all the users
 // export async function getAllUsers({
