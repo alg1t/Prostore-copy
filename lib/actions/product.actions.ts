8a28873082dc8672682ec6794bb7532b3946c39d
Alg1t <alfell88@googlemail.com>
2025-06-07 14:30:54 +0100

    admin2

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,7 +1,7 @@
 "use server";
 import { PrismaClient } from "@prisma/client";
 import { prisma } from "@/db/prisma";
-// import { convertToPlainObject } from "../utils";
+import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 export async function getLatestProducts() {
@@ -40,4 +40,13 @@ export async function getLatestProducts() {
 
 export async function getProductsBySlug(slug: string) {
   return await prisma.product.findFirst({ where: { slug: slug } });
+}
+
+// Get single product by it's ID
+export async function getProductById(productId: string) {
+  const data = await prisma.product.findFirst({
+    where: { id: productId },
+  });
+
+  return convertToPlainObject(data);
 }
