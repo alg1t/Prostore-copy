e6c275b8ec266fb50d0a2b09fdbaddf5b4f72f37
Alg1t <alfell88@googlemail.com>
2025-06-09 11:08:00 +0100

    admin3

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -391,46 +391,46 @@ export async function deleteOrder(id: string) {
   }
 }
 
-// // Update COD order to paid
-// export async function updateOrderToPaidCOD(orderId: string) {
-//   try {
-//     await updateOrderToPaid({ orderId });
+// Update COD order to paid
+export async function updateOrderToPaidCOD(orderId: string) {
+  try {
+    await updateOrderToPaid({ orderId });
 
-//     revalidatePath(`/order/${orderId}`);
+    revalidatePath(`/order/${orderId}`);
 
-//     return { success: true, message: 'Order marked as paid' };
-//   } catch (error) {
-//     return { success: false, message: formatError(error) };
-//   }
-// }
+    return { success: true, message: "Order marked as paid" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
 
-// // Update COD order to delivered
-// export async function deliverOrder(orderId: string) {
-//   try {
-//     const order = await prisma.order.findFirst({
-//       where: {
-//         id: orderId,
-//       },
-//     });
+// Update COD order to delivered
+export async function deliverOrder(orderId: string) {
+  try {
+    const order = await prisma.order.findFirst({
+      where: {
+        id: orderId,
+      },
+    });
 
-//     if (!order) throw new Error('Order not found');
-//     if (!order.isPaid) throw new Error('Order is not paid');
+    if (!order) throw new Error("Order not found");
+    if (!order.isPaid) throw new Error("Order is not paid");
 
-//     await prisma.order.update({
-//       where: { id: orderId },
-//       data: {
-//         isDelivered: true,
-//         deliveredAt: new Date(),
-//       },
-//     });
+    await prisma.order.update({
+      where: { id: orderId },
+      data: {
+        isDelivered: true,
+        deliveredAt: new Date(),
+      },
+    });
 
-//     revalidatePath(`/order/${orderId}`);
+    revalidatePath(`/order/${orderId}`);
 
-//     return {
-//       success: true,
-//       message: 'Order has been marked delivered',
-//     };
-//   } catch (error) {
-//     return { success: false, message: formatError(error) };
-//   }
-// }
+    return {
+      success: true,
+      message: "Order has been marked delivered",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
